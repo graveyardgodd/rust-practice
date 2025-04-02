9c8286fd5eb0d88808733d8d695866157cf6e059
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2025-04-02 10:05:12 +0300

    -- p2

--- src/practice/lesson03.rs
+++ src/practice/lesson03.rs
@@ -1,5 +1,34 @@
 /// console - envelope
+// ******************************
+// *  *                      *  *
+// *    *                  *    *
+// *      *              *      *
+// *        *          *        *
+// *          *      *          *
+// *            *  *            *
+// *             **             *
+// *           *    *           *
+// *         *        *         *
+// *       *            *       *
+// *     *                *     *
+// *   *                    *   *
+// * *                        * *
+// ******************************
+fn envelope(width: u32, height: u32) {
+    let range_y = 1..=height;
+    for y in range_y {
+        for x in 1..=width {
+            let is_hor = y == 1 || y == height;
+            let is_ver = x == 1 || x == width;
+
+            let sym = if is_hor || is_ver { '*' } else { ' ' };
+            print!("{sym}");
+        }
+        print!("\n");
+    }
+}
+
 #[test]
 fn test1() {
-
+    envelope(25, 10);
 }
