216df543cb8011249baa83ac8ccc9491d2e5ba8a
graveyardgodd <stirbnichtvormirr@gmail.com>
2025-04-22 11:52:57 +0300

    combined two methods into one

--- src/homework/hw06.rs
+++ src/homework/hw06.rs
@@ -1,36 +1,21 @@
-fn draw_tree(levels: usize)
- {
+fn draw_tree(levels: usize) {
     let total_levels = levels + 1;
-
-    // max lenght of the last stroke
+ 
+// max bottom stroke lenght
     let max_width = (1..=total_levels).map(|lvl| lvl * 2 - 1).last().unwrap();
 
-    // generating each lvl of triangulars
-    (1..=levels).for_each(|level|
-    {
-        // strokes for each lvl
+ // rendering all lvls
+    (1..=total_levels).for_each(|level| {
         (1..=(2 * level - 1))
             .step_by(2)
-            .for_each(|stars| 
-            {
+            .for_each(|stars| {
                 let padding = (max_width - stars) / 2;
                 let line = " ".repeat(padding) + &"*".repeat(stars);
                 println!("{}", line);
             });
     });
-
-    // main(biggest) triangular
-    (1..=(2 * total_levels - 1))
-        .step_by(2)
-        .for_each(|stars|
-        {
-            let padding = (max_width - stars) / 2;
-            let line = " ".repeat(padding) + &"*".repeat(stars);
-            println!("{}", line);
-        });
 }
 
-fn main()
- {
+fn main() {
     draw_tree(4);
 }
